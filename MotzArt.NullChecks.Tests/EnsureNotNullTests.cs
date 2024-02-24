e22ef4330a98b560336a111f5b7da5dbbe8092f6
Oleg <oshkoderov.oleg@gmail.com>
2024-02-24 12:31:17 +0100

    Add project files.

---
+++ MotzArt.NullChecks.Tests/EnsureNotNullTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace MotzArt.NullChecks.Tests;
+
+[TestFixture]
+public class EnsureNotNullTests
+{
+    [Test]
+    public void ShouldNotThrowWhenStringValueNotNull()
+    {
+        var value = "";
+        value.EnsureNotNull().Should().BeSameAs(value);
+    }
+
+    [Test]
+    public void ShouldNotThrowWhenObjectValueNotNull()
+    {
+        var obj = new object();
+
+        obj.EnsureNotNull().Should().BeSameAs(obj);
+    }
+}
---
+++ MotzArt.NullChecks/NullCheckExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace MotzArt.NullChecks;
+
+public static class NullCheckExtensions
+{
+    [return: NotNull]
+    public static T EnsureArgumentNotNull<T>([NotNull] this T? obj, [CallerArgumentExpression("obj")] string name = "<not specified>")
+    {
+        return obj ?? throw new ArgumentNullException(name);
+    }
+
+    public static T EnsureArgumentNotNull<T>([NotNull] this T? obj, [CallerArgumentExpression("obj")] string name = "<not specified>") where T : struct
+    {
+        return obj ?? throw new ArgumentNullException(name);
+    }
+
+    [return: NotNull]
+    public static T EnsureNotNull<T>([NotNull] this T? obj, [CallerArgumentExpression("obj")] string name = "<not specified>")
+    {
+        return obj ?? throw new NullReferenceException($"{name} should not be null.");
+    }
+
+    public static T EnsureNotNull<T>([NotNull] this T? obj, [CallerArgumentExpression("obj")] string name = "<not specified>") where T : struct
+    {
+        return obj ?? throw new NullReferenceException($"{name} should not be null.");
+    }
+
+    [return: NotNull]
+    public static List<TItem> EnsureNotEmpty<TItem>([NotNull] this List<TItem>? list, [CallerArgumentExpression("list")] string name = "<not specified>")
+    {
+        if (list == null || list.Count == 0) throw new ArgumentException($"{name} should have at least one item.");
+        return list;
+    }
+
+    [return: NotNull]
+    public static IReadOnlyList<TItem> EnsureNotEmpty<TItem>([NotNull] this IReadOnlyList<TItem>? list, [CallerArgumentExpression("list")] string name = "<not specified>")
+    {
+        if (list == null || list.Count == 0) throw new ArgumentException($"{name} should have at least one item.");
+        return list;
+    }
+
+    [return: NotNull]
+    public static IList<TItem> EnsureNotEmpty<TItem>([NotNull] this IList<TItem>? list, [CallerArgumentExpression("list")] string name = "<not specified>")
+    {
+        if (list == null || list.Count == 0) throw new ArgumentException($"{name} should have at least one item.");
+        return list;
+    }
+}
