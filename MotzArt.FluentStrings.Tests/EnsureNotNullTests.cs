3b34aff54e300a48379838a0e4863f2be637fbc4
Oleg <oshkoderov.oleg@gmail.com>
2024-03-03 19:20:28 +0100

    Move EnsureNotNullTests to separate file.

---
+++ MotzArt.FluentStrings.Tests/EnsureNotNullTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace MotzArt.FluentStrings.Tests;
+
+[TestFixture]
+public class EnsureNotNullTests
+{
+    [Test]
+    [TestCase("a")]
+    [TestCase(" value")]
+    [TestCase("value ")]
+    [TestCase("some string")]
+    public void ShouldNotThrowIfNotNullOrWhitespace(string value)
+    {
+        value.EnsureHasValue().Should().BeSameAs(value);
+    }
+
+    [Test]
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase(" ")]
+    [TestCase("    ")]
+    [TestCase("\t")]
+    [TestCase("\n")]
+    [TestCase("\r\n")]
+    public void ShouldThrowIfNullOrWhitespace(string? value)
+    {
+        var act = () => value.EnsureHasValue();
+        act.Should().Throw<ArgumentException>().WithMessage($"{nameof(value)} should not be null or white-space.");
+    }
+}
--- MotzArt.FluentStrings.Tests/StringExtensionsTests.cs
+++ MotzArt.FluentStrings.Tests/StringExtensionsTests.cs
@@ -49,31 +49,3 @@ public class StringExtensionsTests
         str.HasValue().Should().Be(expected);
     }
 }
-
-[TestFixture]
-public class EnsureNotNullTests
-{
-    [Test]
-    [TestCase("a")]
-    [TestCase(" value")]
-    [TestCase("value ")]
-    [TestCase("some string")]
-    public void ShouldNotThrowIfNotNullOrWhitespace(string value)
-    {
-        value.EnsureHasValue().Should().BeSameAs(value);
-    }
-
-    [Test]
-    [TestCase(null)]
-    [TestCase("")]
-    [TestCase(" ")]
-    [TestCase("    ")]
-    [TestCase("\t")]
-    [TestCase("\n")]
-    [TestCase("\r\n")]
-    public void ShouldThrowIfNullOrWhitespace(string? value)
-    {
-        var act = () => value.EnsureHasValue();
-        act.Should().Throw<ArgumentException>().WithMessage($"{nameof(value)} should not be null or white-space.");
-    }
-}
